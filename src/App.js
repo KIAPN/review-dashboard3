6565c13d3e9a83c6a3ec9b3a44ddf5df220962aa
KIAPN <158529417+KIAPN@users.noreply.github.com>
2025-02-25 21:27:22 -0500

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReviewDashboard from './components/ReviewDashboard';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ReviewDashboard />
+    </div>
+  );
+}
+
+export default App;
